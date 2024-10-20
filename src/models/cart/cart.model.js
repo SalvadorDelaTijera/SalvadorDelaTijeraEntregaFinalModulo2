4f4ee35104e9d3518e19e658d4f1142f30415261
Salvador De la Tijera <salvagerar@gmail.com>
2024-10-20 10:54:48 -0600

    Se creó el modelo del Carrito y el esquema de CartItem.

---
+++ src/models/cart/cart.model.js
@@ -0,0 +1,71 @@
+import { Schema, model } from "mongoose";
+import MongoosePaginate from "mongoose-paginate-v2";
+import MongooseDelete from "mongoose-delete";
+
+export const CartItemSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  salesPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+    default: 1,
+  },
+},{
+  timestamps: true,
+  _id: false,
+  virtuals: {
+    subtotal: {
+      get() {
+        return this.salesPrice * this.quantity;
+      },
+    },
+  },
+  toObject: {
+    getters: true,
+    virtuals: true,
+  }
+});
+
+export const CartSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true,
+  },
+  items: [CartItemSchema],
+}, {
+  timestamps: true,
+  virtuals: {
+    total: {
+      get() {
+        return this.items.reduce((total, item) => total += item.salesPrice * item.quantity, 0);
+      },
+    },
+  },
+  toObject: {
+    getters: true,
+    virtuals: true,
+  }
+});
+
+CartSchema.plugin(MongoosePaginate);
+
+CartSchema.plugin(MongooseDelete, {
+  indexFields: ["delete", "deletedAt"],
+  overrideMethods: "all",
+});
+
+const Cart = model('Cart', CartSchema);
+
+export default Cart;
