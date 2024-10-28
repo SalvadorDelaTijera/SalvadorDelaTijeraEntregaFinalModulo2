feab72261bd249521e3eff10617f064d9f6b266d
Salvador De la Tijera <salvagerar@gmail.com>
2024-10-28 15:06:01 -0600

    Created User's constants and updated DTO's accordingly.

--- src/models/user/dtos/create-user.dto.js
+++ src/models/user/dtos/create-user.dto.js
@@ -1,61 +1,36 @@
-import Joi from 'joi';
+import { hash } from 'bcrypt';
+import { createUserSchema, USER_PASSWORD_SALT_ROUNDS } from '../../../constants/user.constants.js';
 
 export default class CreateUserDTO {
-  static from(object) {
+  static async from(object) {
     try {
-      const validation = CreateUserDTO.validate(object);
+      const validation = createUserSchema.validate(object);
 
-      if (!validation.error) {
-        object.roles = object.roles.map((role) => role.toLowerCase());
-
-        if (object.roles.length === 0 && !object.roles.includes("user")) {
-          object.roles.push("user");
-        }
-
-        return {
-          email: object.email,
-          password: object.password,
-          firstName: object.firstName,
-          lastName: object.lastName,
-          roles: object.roles,
-        };
+      if (validation.error) {
+        throw new Error(Array.toString(validation.error.details));
       }
 
-      throw new Error(Array.toString(validation.error.details));
+      const password = await hash(object.password, USER_PASSWORD_SALT_ROUNDS);
+
+      const roles = object.roles.map((role) => role.toLowerCase());
+
+      if (roles.length === 0 && !roles.includes("user")) {
+        roles.push("user");
+      }
+
+      return {
+        email: object.email,
+        password,
+        firstName: object.firstName,
+        lastName: object.lastName,
+        roles,
+      };
     } catch (error) {
       throw new Error(error.message);
     }
   }
 
   static validate(object) {
-    const schema = Joi.object({
-      email: Joi.string()
-        .required()
-        .email({ minDomainSegments: 2 })
-        .message("Debe proporcionar un email."),
-      password: Joi.string()
-        .required()
-        .pattern(
-          /^(?=.*[a-zA-ZñÑ])(?=.*\d)(?=.*[!@#$%^&*()\-_=+[\]{};:'",.<>?/\\|`~])[A-Za-zñÑ\d!@#$%^&*()\-_=+[\]{};:'",.<>?/\\|`~]{6,}$/
-        )
-        .message(
-          "Debe proporcionar una contraseña de, por lo menos, 6 caracteres y debe contener por lo menos una letra, un número y un símbolo."
-        ),
-      firstName: Joi.string()
-        .required()
-        .min(1)
-        .message(
-          "Debe proporcionar el/los nombre(s) de, por lo menos, 1 caracter."
-        ),
-      lastName: Joi.string()
-        .required()
-        .min(1)
-        .message(
-          "Debe proporcionar el/los apellido(s) de, por lo menos, 1 caracter."
-        ),
-      roles: Joi.array().items(Joi.string().label("Role")),
-    });
-
-    return schema.validate(object);
+    return createUserSchema.validate(object);
   }
 }
